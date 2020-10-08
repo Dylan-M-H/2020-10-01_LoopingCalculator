179f5704f82eb503abeb214952dc7979b204d3fc
Dylan-M-H <dmhosmer@outlook.com>
2020-10-07 22:37:52 -0500

    Initial Commmit

---
+++ 2020-10-01_LoopingCalculator.c
@@ -0,0 +1,14 @@
+/*
+2020-10-01_LoopingCalculator.c
+Author: Dylan M. Hosmer
+Created On: 07OCT20
+Last Modified: 07OCT20
+*/
+
+#include <stdio.h>
+
+
+int main() {
+   printf("\n2020-10-01_LoopingCalculator is alive!\n\n");
+   return 0;
+}
